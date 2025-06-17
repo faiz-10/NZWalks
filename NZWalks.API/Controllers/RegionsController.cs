8e9efc9318f1f728edeecafa25db77618120528e
Faizan Ali <faizanpali10@gmail.com>
2025-06-17 12:06:08 +0530

    Add project files.

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // GET: api/regions
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            // Get data from the database (Doamin models)
+            var regions = await dbContext.Regions.ToListAsync();
+
+            // Map domain models to DTOs (if necessary)
+            var regionsDto = new List<RegionDto>();
+            foreach(var region in regions)
+            {
+                regionsDto.Add(new RegionDto
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    RegionImageURL = region.RegionImageURL
+                });
+            }
+
+
+            // Return DTOs
+            return Ok(regionsDto);
+        }
+
+        // GET: api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            //var region = dbContext.Regions.Find(id);
+
+            // Get data from the database (Domain model)
+            var region = await dbContext.Regions.FirstOrDefaultAsync(r => r.Id == id);
+
+            if (region is null)
+            {
+                return NotFound();
+            }
+
+            // Map domain model to DTO (if necessary)
+            var regionDto = new RegionDto
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageURL = region.RegionImageURL
+            };
+
+            // Return DTO back to the client
+            return Ok(regionDto);
+        }
+
+        // POST: api/regions
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            // Map DTO to Domain model
+            var regionDomainModel = new Region
+            {
+                Name = addRegionRequestDto.Name,
+                Code = addRegionRequestDto.Code,
+                RegionImageURL = addRegionRequestDto.RegionImageURL
+            };
+
+            // Use Domain model to create a new region in the database
+            await dbContext.Regions.AddAsync(regionDomainModel);
+            await dbContext.SaveChangesAsync();
+
+            // Map Domain model back to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageURL = regionDomainModel.RegionImageURL
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = regionDomainModel.Id}, regionDto);
+        }
+
+        // PUT: api/regions/{id}
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
+        {
+            // check if the region exists
+            var regionDomainModel = await dbContext.Regions.FirstOrDefaultAsync(r => r.Id == id);
+
+            if(regionDomainModel is null)
+            {
+                return NotFound();
+            }
+
+            // Map DTO to Domain model
+            regionDomainModel.Code = updateRegionRequestDto.Code;
+            regionDomainModel.Name = updateRegionRequestDto.Name;
+            regionDomainModel.RegionImageURL = updateRegionRequestDto.RegionImageURL;
+
+            await dbContext.SaveChangesAsync();
+
+            // Map Domain model back to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageURL = regionDomainModel.RegionImageURL,
+            };
+
+            return Ok(regionDto);
+
+        }
+
+        // DELETE: api/regions/{id}
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var regionDomainModel = await dbContext.Regions.FirstOrDefaultAsync(r => r.Id == id);
+
+            if(regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            // Delete the region from the database
+            dbContext.Regions.Remove(regionDomainModel);
+            await dbContext.SaveChangesAsync();
+
+            // return deleted region if desired
+            // Mapping Domain model to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageURL = regionDomainModel.RegionImageURL
+            };
+
+            return Ok(regionDto);
+        }
+    }
+}
---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    //https://localhost:5001/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] students = new string[] { "John Doe", "Jane Smith", "Alice Johnson" };
+
+            return Ok(students);
+        }
+    }
+}
